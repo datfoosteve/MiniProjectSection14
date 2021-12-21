fc4bfaff1b5dab410dd73cadc3b1b98798879598
Stephen Puthenpurackal <datfoosteve@gmail.com>
2021-12-21 11:55:27 -0600

    some more folders

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const handlebars = require('express-handlebars')
 const session = require('express-session');
 const routes = require('./controllers');
 
 const sequelize = require('./config/connection');
+const req = require('express/lib/request');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
